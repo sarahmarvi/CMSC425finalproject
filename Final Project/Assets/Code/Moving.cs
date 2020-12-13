2882060fbf37d4ec17c0a2edaa9ba0f280da5bd4
leo liu <qq279699450@gmail.com>
2020-12-12 20:46:31 -0500

    update turn esc

--- Final Project/Assets/Code/Moving.cs
+++ Final Project/Assets/Code/Moving.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     public float sensitivity = 0.3f;
+    public float speed = 5;
     //Rigidbody rb;
     Vector3 initialPos;
     Quaternion initialQuaternion;
@@ -14,10 +15,47 @@ public class Moving : MonoBehaviour
         //rb = GetComponent<Rigidbody>();
         initialPos = transform.position;
         initialQuaternion = transform.rotation;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     private void Update()
     {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        }
+
+        transform.Rotate(0, speed * Time.deltaTime * Input.GetAxis("Mouse X"), 0, Space.World);
+        /*
+        float input = Input.GetAxis("Mouse Y");
+
+        float rot = transform.localEulerAngles.x + (-speed * Time.deltaTime * input);
+
+        if (rot > 45 && rot < 315 && input != 0)
+        {
+            if (input < 0)
+            {
+                rot = 45;
+            }
+            else
+            {
+                rot = 315;
+            }
+
+        }
+
+        Debug.Log(rot);
+        transform.localEulerAngles = new Vector3(rot, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        */
+        //Debug.Log(transform.localEulerAngles.x);
+
+
         if (Input.GetKey(KeyCode.R))
         {
             transform.rotation = initialQuaternion;
